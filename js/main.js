fc98ece86120cb1ee8399790c62f540f53156651
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-02-26 16:29:31 -0500

    basic js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-var $moveDownEntry = $('.move-down');
-var $form = $('form');
-var $input = $('#to-do');
+var $toDoList = $('.todo');
 
-$moveDownEntry.on('click', function () {
-    var currentLoc = $to-Do.offset();
-    
-    $to-Do.css('top', currentLoc.top + 10); 
+var $moveDownButton = $('.move-down');
+var $form = $('form');
+var $input = $('#list');
+
