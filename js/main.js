622558a565e1df7e8678a3219c26a5cc49d69ed7
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-03-02 12:22:23 -0500

    working numbered list

--- js/main.js
+++ js/main.js
@@ -3,23 +3,19 @@ var $toDoList = $('.todo');
 var $moveDownButton = $('.move-down');
 var $form = $('form');
 var $input = $('#list');
+var $li = $('.li');
+var $ol = $('.ol');
 
 $form.on('submit', function(eventobject) {
     eventobject.preventDefault();
     
-    $toDoList.css('todo', $input.val());
+    $ol.css('ol', $input.val());
     
-    var $theH1 = $('<h1>');
-    $theH1.html($input.val());
+    var $li = $('<li>');
+    $li.html($input.val());
     
-    $toDoList.prepend($theH1)
+    $ol.prepend($li)
     
-    $input.val('');
-    
+    $input.val('');    
 });
 
-//$theH1.on('click' function () {
-
-  //  $theH1.css('stroke')
-    
-//});
